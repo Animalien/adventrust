17c003a1aa3eb845454871953ffd56ba0e57562c
Animalien <Animalien@users.noreply.github.com>
2025-08-13 13:31:52 -0700

    2015_01 Part 2

--- advent2015_01/src/main.rs
+++ advent2015_01/src/main.rs
@@ -2,8 +2,11 @@ use std::string::String;
 
 use adventlib::*;
 
-fn process_line(line: &str) -> i32 {
-    let mut floor = 0;
+fn process_line(line: &str) -> (i32, Option<usize>) {
+    let mut floor: i32 = 0;
+    let mut entered_basement_index: Option<usize> = None;
+
+    let mut char_index: usize = 1;
 
     for c in line.chars() {
         match c {
@@ -15,9 +18,15 @@ fn process_line(line: &str) -> i32 {
             }
             _ => panic!("Malformed line: {}", line)
         }
+
+        if entered_basement_index == None && floor < 0 {
+            entered_basement_index = Some(char_index);
+        }
+
+        char_index = char_index + 1;
     }
 
-    floor
+    (floor, entered_basement_index)
 }
 
 fn process(filename: &str) {
@@ -31,14 +40,23 @@ fn process(filename: &str) {
 
     println!("Processing contents of {}", filename);
     for line in lines {
-        let floor = process_line(&line);
+        let (floor, entered_basement_index) = process_line(&line);
         let line_to_print = if line.len() > 20 {
             &(String::from(&line[..20]) + "...")
         } else {
             &line
         };
 
-        println!("  Line '{line_to_print}' takes us to {floor}");
+        println!("  Line '{line_to_print}' takes us to {floor}, and basement was entered at char index {}",
+            match entered_basement_index {
+                Some(value) => {
+                    value.to_string()
+                }
+                _ => {
+                    "None".to_string()
+                }
+            }
+        );
     }
 }
 

c4bd0cd540bbf38e204808add05f54f79a7502c0
Animalien <Animalien@users.noreply.github.com>
2025-08-14 16:06:04 -0700

    2015_03 Parts 1 and 2

--- advent2015_03/src/main.rs
+++ advent2015_03/src/main.rs
@@ -42,7 +42,65 @@ fn process(filename: &str) {
             house_set.insert((x, y));
         }
 
-        println!("  In line '{line}', delivered presents to {} houses", house_set.len());
+        let mut x1 = 0i32;
+        let mut y1 = 0i32;
+        let mut x2 = 0i32;
+        let mut y2 = 0i32;
+
+        let mut house_set2 = HashSet::<(i32, i32)>::new();
+        house_set2.insert((0,0));
+
+        let mut robo_santa = false;
+
+        for c in line.chars() {
+            if robo_santa {
+                match c {
+                    '^' => {
+                        y1 += 1;
+                    }
+                    '<' => {
+                        x1 -= 1;
+                    }
+                    '>' => {
+                        x1 += 1;
+                    }
+                    'v' => {
+                        y1 -= 1;
+                    }
+                    _ => {
+                        panic!("Invalid direction character in data:  {c}");
+                    }
+                }
+
+                house_set2.insert((x1, y1));
+
+                robo_santa = false;
+            } else {
+                match c {
+                    '^' => {
+                        y2 += 1;
+                    }
+                    '<' => {
+                        x2 -= 1;
+                    }
+                    '>' => {
+                        x2 += 1;
+                    }
+                    'v' => {
+                        y2 -= 1;
+                    }
+                    _ => {
+                        panic!("Invalid direction character in data:  {c}");
+                    }
+                }
+
+                house_set2.insert((x2, y2));
+
+                robo_santa = true;
+            }
+        }
+
+        println!("  In line '{}', delivered presents to {} houses with just Santa, delivered presents to {} houses with Santa plus Robo Santa", maxed_string(&line, 20), house_set.len(), house_set2.len());
     }
 }
 
--- adventlib/src/lib.rs
+++ adventlib/src/lib.rs
@@ -5,3 +5,16 @@ use std::string::String;
 pub fn read_file_lines(filename: &str) -> Result<Vec<String>> {
     Ok(read_to_string(filename)?.lines().map(String::from).collect())
 }
+
+pub fn maxed_string(in_string: &str, max_len: usize) -> String {
+    let mut out_string: String;
+    if in_string.len() > max_len {
+        out_string = (&in_string[..max_len]).to_string();
+        out_string += "...";
+    } else {
+        out_string = in_string.to_string();
+    }
+
+    out_string
+}
+

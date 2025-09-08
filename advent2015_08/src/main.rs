f0c230600b67d0653444f3ca5ccf45f4997018ba
Animalien <Animalien@users.noreply.github.com>
2025-09-08 15:27:58 -0700

    2015_08 - Part 2

--- advent2015_08/src/main.rs
+++ advent2015_08/src/main.rs
@@ -12,43 +12,66 @@ fn process(filename: &str, verbose: bool) {
     println!("Processing contents of {}", filename);
 
     let mut total_num_input_chars = 0usize;
-    let mut total_num_output_chars = 0usize;
+    let mut total_num_output_chars1 = 0usize;
+    let mut total_num_output_chars2 = 0usize;
     for line in lines {
         let mut num_input_chars = 2usize;  // for initial and final "
-        let mut num_output_chars = 0usize;
-        let mut char_iter = line[1..line.len()-1].chars();
+        let mut num_output_chars1 = 0usize;
+        let mut char_iter1 = line[1..line.len()-1].chars();
 
-        while let Some(ch1) = char_iter.next() {
+        while let Some(ch1) = char_iter1.next() {
             if ch1 == '\\' {
-                match char_iter.next().unwrap() {
+                match char_iter1.next().unwrap() {
                     '\\' | '\"' => {
                         num_input_chars += 2;
-                        num_output_chars += 1;
+                        num_output_chars1 += 1;
                     },
                     'x' => {
-                        char_iter.next().unwrap();
-                        char_iter.next().unwrap();
+                        char_iter1.next().unwrap();
+                        char_iter1.next().unwrap();
                         num_input_chars += 4;
-                        num_output_chars += 1;
+                        num_output_chars1 += 1;
                     }
                     _ => panic!("unexpected escape sequence!")
                 }
-
             } else {
                 num_input_chars += 1;
-                num_output_chars += 1;
+                num_output_chars1 += 1;
             }
         }
 
+        let mut num_output_chars2 = 2usize;  // for initial and final "
+        let mut char_iter2 = line.chars();
+        let mut new_st = "\"".to_string();
+        while let Some(ch) = char_iter2.next() {
+            match ch {
+                '\"' => {
+                    num_output_chars2 += 2;
+                    new_st += "\\\"";
+                },
+                '\\' => {
+                    num_output_chars2 += 2;
+                    new_st += "\\\\";
+                },
+                _ => {
+                    num_output_chars2 += 1;
+                    new_st.push(ch);
+                }
+            }
+        }
+        new_st.push('\"');
+
         if verbose {
-            println!("    For line {line}, num input chars = {num_input_chars}, num output chars = {num_output_chars}");
+            println!("    For line {line}, num input chars = {num_input_chars}, num output chars 1 = {num_output_chars1}, num output chars 2 = {num_output_chars2}, new st = {new_st}");
         }
 
         total_num_input_chars += num_input_chars;
-        total_num_output_chars += num_output_chars;
+        total_num_output_chars1 += num_output_chars1;
+        total_num_output_chars2 += num_output_chars2;
     }
 
-    println!("  Total number of input chars = {total_num_input_chars}; total num output chars = {total_num_output_chars}; total diff = {}", total_num_input_chars - total_num_output_chars);
+    println!("  Total number of input chars = {total_num_input_chars}; total num output chars 1 = {total_num_output_chars1}; total diff 1 = {}; total num output chars 2 = {total_num_output_chars2}; total diff 2 = {}",
+        total_num_input_chars - total_num_output_chars1, total_num_output_chars2 - total_num_input_chars);
 }
 
 fn main() {

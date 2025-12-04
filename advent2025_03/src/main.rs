7166a24e93e12550bd0225292ca0f1f25ca6b04a
Animalien <Animalien@users.noreply.github.com>
2025-12-04 15:28:49 -0800

    2025_03 - Part 2

--- advent2025_03/src/main.rs
+++ advent2025_03/src/main.rs
@@ -1,6 +1,33 @@
 use adventlib::*;
 
-fn process(filename: &str, verbose: bool) {
+fn voltage_from_string(string: &str, num_chars: usize) -> usize {
+    if string == "811111111111119" && num_chars == 12 {
+        println!("THE PROBLEMO")
+    }
+
+    let mut next_char_index = 0usize;
+    let mut num_chars_remaining = num_chars;
+    let mut voltage = 0usize;
+
+    while num_chars_remaining > 0 {
+        let num_chars_to_check = string.len() - next_char_index - num_chars_remaining + 1;
+        let mut char_indices_iter = string.char_indices().skip(next_char_index).take(num_chars_to_check).peekable();
+        if char_indices_iter.peek().is_none() {
+            println!("    WE GOT NUN!  next_char_index = {next_char_index}, num_chars_remaining = {num_chars_remaining}, num_chars_to_check = {num_chars_to_check}, string = {string}, voltage = {voltage}");
+        }
+        assert!(!char_indices_iter.peek().is_none());
+        let best_next_char_tuple = char_indices_iter.max_by(|lhs, rhs| if lhs.1 == rhs.1 { rhs.0.cmp(&lhs.0) } else { lhs.1.cmp(&rhs.1) }).unwrap();
+        let best_next_char = best_next_char_tuple.1;
+        next_char_index = best_next_char_tuple.0 + 1;
+        num_chars_remaining -= 1;
+
+        voltage = voltage * 10usize + best_next_char.to_digit(10).unwrap() as usize;
+    }
+
+    voltage
+}
+
+fn process(filename: &str, num_chars: usize, verbose: bool) {
     let lines = match read_file_lines(filename) {
         Err(err) => {
             println!("process failed: {:?}", err);
@@ -9,23 +36,12 @@ fn process(filename: &str, verbose: bool) {
         Ok(val) => val
     };
 
-    println!("Processing contents of {}", filename);
+    println!("Processing contents of {}, using {num_chars} chars", filename);
 
     let mut sum_voltages = 0usize;
 
     for line in lines {
-        let mut line_voltage = 0usize;
-        for ch_tuple in line.char_indices() {
-            if ch_tuple.0 < line.len() -1 {
-                let largest_next_char = &line[(ch_tuple.0+1)..].chars().max().unwrap();
-
-                let this_voltage = (ch_tuple.1.to_digit(10).unwrap() as usize) * 10usize + largest_next_char.to_digit(10).unwrap() as usize;
-
-                if this_voltage > line_voltage {
-                    line_voltage = this_voltage;
-                }
-            }
-        }
+        let line_voltage = voltage_from_string(&line, num_chars);
 
         if verbose {
             println!("  in line {line}, largest voltage found is {line_voltage}")
@@ -38,6 +54,8 @@ fn process(filename: &str, verbose: bool) {
 }
 
 fn main() {
-    process("Example.txt", true);
-    process("Input.txt", false);
+    process("Example.txt", 2, true);
+    process("Input.txt", 2, false);
+    process("Example.txt", 12, true);
+    process("Input.txt", 12, false);
 }

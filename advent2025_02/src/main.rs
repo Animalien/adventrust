bb4aa81680e1fc6893519ab17c683a010ea7e608
Animalien <Animalien@users.noreply.github.com>
2025-12-04 10:53:27 -0800

    2025_02 - Part 2

--- advent2025_02/src/main.rs
+++ advent2025_02/src/main.rs
@@ -1,32 +1,52 @@
 use std::fs::read_to_string;
 
-fn is_id_valid(id: &str) -> bool {
+fn id_contains_repeats(id: &str, num_repeats: usize) -> bool {
     let id_len = id.len();
-    if id_len.is_multiple_of(2) {
-        let half_id_len = id_len / 2;
-        &id[..half_id_len] != &id[half_id_len..]
-    } else {
+    if id_len.is_multiple_of(num_repeats) {
+        let chunk_id_len = id_len / num_repeats;
+        for next_chunk_index in 1..num_repeats {
+            if &id[0..chunk_id_len] != &id[(next_chunk_index * chunk_id_len)..((next_chunk_index + 1) * chunk_id_len)] {
+                return false;
+            }            
+        }
+
         true
+    } else {
+        false
     }
 }
 
-fn process(filename: &str, verbose: bool) {
-    println!("Processing contents of {}", filename);
+fn is_id_valid(id: &str, new_rules: bool) -> bool {
+    if new_rules {
+        for num_repeats in 2..=id.len() {
+            if id_contains_repeats(id, num_repeats) {
+                return false;
+            }
+        }
+
+        true
+    } else {
+        !id_contains_repeats(id, 2)
+    }
+}
+
+fn process(filename: &str, new_rules: bool, verbose: bool) {
+    println!("Processing contents of {}, {} new rules", filename, if new_rules { "using" } else { "NOT USING" });
 
     let contents = read_to_string(filename).unwrap();
     let ranges = contents.split(&[',', '\r', '\n'][..]).filter(|s| !s.is_empty());
 
-    let mut invalid_id_sum = 0u64;
+    let mut invalid_id_sum = 0usize;
 
     for range in ranges {
         let mut endpoints = range.split('-');
 
-        let first = endpoints.next().unwrap().parse::<u64>().unwrap();
-        let second = endpoints.next().unwrap().parse::<u64>().unwrap();
+        let first = endpoints.next().unwrap().parse::<usize>().unwrap();
+        let second = endpoints.next().unwrap().parse::<usize>().unwrap();
 
         for id in first..=second {
             let id_string = id.to_string();
-            if is_id_valid(&id_string) {
+            if is_id_valid(&id_string, new_rules) {
                 if verbose {
                     println!("  {id_string} is valid")
                 }
@@ -44,6 +64,8 @@ fn process(filename: &str, verbose: bool) {
 }
 
 fn main() {
-    process("Example.txt", true);
-    process("Input.txt", false);
+    process("Example.txt", false, false);
+    process("Input.txt", false, false);
+    process("Example.txt", true, false);
+    process("Input.txt", true, false);
 }
